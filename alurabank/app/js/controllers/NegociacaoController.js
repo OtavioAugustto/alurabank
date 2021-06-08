008d2a902e210550c3b9e2500b52b7d603e298c3
OtavioAugustto <otavioaugustto93@gmail.com>
2021-06-08 17:22:52 -0300

    Implement and define template string on NegotiationsView

--- alurabank/app/js/controllers/NegociacaoController.js
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -1,13 +1,17 @@
 class NegociacaoController {
     constructor() {
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputaValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault();
         const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputaValor.value));
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
     }
 }
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,8 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputaValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor(){
 
@@ -10,7 +12,8 @@ class NegociacaoController {
 
         this._inputData = <HTMLInputElement>$('#data');
         this._inputQuantidade = <HTMLInputElement>$('#quantidade');
-        this._inputaValor = <HTMLInputElement>$('#valor');
+        this._inputaValor = <HTMLInputElement>$('#valor');    
+        this._negociacoesView.update(this._negociacoes);  
     }
 
     adiciona(event: Event){
@@ -24,6 +27,8 @@ class NegociacaoController {
             parseFloat(this._inputaValor.value),
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        
+        this._negociacoesView.update(this._negociacoes);  
     }
 }
